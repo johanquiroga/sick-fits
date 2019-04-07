6eaf6635b557c073fc889f7d39c7d8cb0b5d25ab
Johan Quiroga <johan.c.quiroga@gmail.com>
2019-04-06 19:14:41 -0500

    feat: added delete item functionality

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -15,6 +15,15 @@ const Mutations = {
     // run the update method
     return ctx.db.mutation.updateItem({ data: updates, where: { id: args.data.id } }, info);
   },
+  async deleteItem(parent, args, ctx, info) {
+    const { where } = args;
+    // 1. Find the item
+    const item = await ctx.db.query.item({ where }, '{ id title }');
+    // 2. Check if they own that item, or have the permissions
+    // TODO
+    // 3. Delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
 };
 
 module.exports = Mutations;
--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -6,6 +6,8 @@ import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 
+import DeleteItem from './DeleteItem';
+
 import formatMoney from '../lib/formatMoney';
 
 export default class Item extends Component {
@@ -40,7 +42,7 @@ export default class Item extends Component {
             <a>Edit ✏️</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
     );
