2ec0249dc45fd24e61fdaab4fa65bc26f66fc390
Johan Quiroga <johan.c.quiroga@gmail.com>
2019-04-14 16:31:44 -0500

    feat: added endpoint to retrieve logged in user data

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 // let's go!
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 
@@ -9,7 +10,17 @@ const db = require('./db');
 const server = createServer();
 
 server.express.use(cookieParser());
-// TODO use express middleware to populate current user
+
+// decode the JWT so we can get the user id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the `req` for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
